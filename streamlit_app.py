b5fcc2d7d3d67cc53204a7ebf9081259a6473835
karthi <108882796+kar7hi@users.noreply.github.com>
2022-07-08 20:43:32 +0530

    Update streamlit_app.py
    changed code to display fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,10 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST)")
+my_data_rows = my_cur.fetchall()
+streamlit.header("This Fruit Load List Contains:")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
