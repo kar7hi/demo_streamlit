aacee27cb23b2b9d345224dc3213bae2cb2ecdd2
karthi <108882796+kar7hi@users.noreply.github.com>
2022-07-08 21:05:31 +0530

    Update streamlit_app.py
    added exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,18 @@ streamlit.dataframe(fruits_to_show)
 
 # Display Fruityvice request
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize json message
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output normalize data to screen
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit choice: 
+      streamlit.error("P1ease select a fruit to get information.")
+  else: 
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # normalize json message
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # output normalize data to screen
+      streamlit.dataframe(fruityvice_normalized)
+except URL Error as e: 
+  streamlit.error() 
 
 streamlit.stop()
 import snowflake.connector
