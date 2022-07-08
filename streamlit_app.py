960ca171c928fd45bd6e55a8732c3ec7c7c6d839
karthi <108882796+kar7hi@users.noreply.github.com>
2022-07-08 20:47:55 +0530

    Update streamlit_app.py
    insert to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,15 +33,16 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output normalize data to screen
 streamlit.dataframe(fruityvice_normalized)
 
-
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST)")
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("This Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding fruit ', add_my_fruit)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
