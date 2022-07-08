42315bece3b464e2bd153a7f199281868f28d090
karthi <108882796+kar7hi@users.noreply.github.com>
2022-07-08 20:50:40 +0530

    Update streamlit_app.py
    added error package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 
+from urllib.error import URLError
 streamlit.title('My Parents New Healthly Dinner')
 
 streamlit.header('Breakfast Favorites')
@@ -25,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize json message
@@ -33,6 +35,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output normalize data to screen
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
